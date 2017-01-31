80b4f4ffc5d7d779d497e21565c53ff22c54522f
V-Rod <pjvelazquez@me.com>
2017-01-31 15:15:49 -0600

    my-adlister -mvc exercise -LoginServlet(added the general functionality of a User class and move credential validation to servlet)

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -1,5 +1,36 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by V-Rod on 1/31/17.
  */
-public class LoginServlet {
+@WebServlet(name = "LoginServlet", urlPatterns = {"/login"})
+public class LoginServlet extends HttpServlet {
+    protected void doPost(
+            HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+
+        User user = new User();
+        user.setUsername(request.getParameter("username"));
+        user.setPassword(request.getParameter("password"));
+
+        if ("admin".equalsIgnoreCase(user.getUsername()) &&
+                "password".equalsIgnoreCase(user.getPassword())) {
+            request.setAttribute("user", user);
+            request.getRequestDispatcher("profile.jsp").forward(request, response);
+        } else {
+            response.sendRedirect("/login");
+        }
+    }
+
+    protected void doGet(
+            HttpServletRequest request,
+            HttpServletResponse response) throws ServletException, IOException {
+
+        request.getRequestDispatcher("login.jsp").forward(request, response);
+    }
 }
