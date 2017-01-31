dd298d3cd356521e3965608ef45ab1a4e915636b
V-Rod <pjvelazquez@me.com>
2017-01-31 14:56:39 -0600

    my-adlister -mvc exercise -AdsIndexServlet(added code to file to set attribute and requestDispatcher)

--- src/main/java/AdsIndexServlet.java
+++ src/main/java/AdsIndexServlet.java
@@ -1,5 +1,18 @@
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
 /**
  * Created by V-Rod on 1/31/17.
  */
-public class AdsIndexServlet {
+@WebServlet(name="AdsIndexServlet", urlPatterns = "/ads")
+public class AdsIndexServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setAttribute("ads", DaoFactory.getAdsDao().all());
+        req.getRequestDispatcher("/ads/index.jsp").forward(req, resp);
+
+    }
 }
